deba2938e83ad6b0a2a5cd7770f58957a4365954
austinmusebe <austinmusebe@gmail.com>
2024-10-26 11:43:14 +0300

    added the skibidi

--- app/src/main/java/com/example/mobprogproject/MainActivity.java
+++ app/src/main/java/com/example/mobprogproject/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.mobprogproject;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +18,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        EditText testing;
+        ImageView pic;
+
+        testing=findViewById(R.id.editTxt1);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+
+
         });
+
     }
 }
