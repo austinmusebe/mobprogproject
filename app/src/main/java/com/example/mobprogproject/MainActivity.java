699e11d542593f0662cc36efe4c69d48f443650d
david <daverarieya@gmail.com>
2024-10-29 23:44:26 +0300

    testing

--- app/src/main/java/com/example/mobprogproject/MainActivity.java
+++ app/src/main/java/com/example/mobprogproject/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.mobprogproject;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,19 +14,22 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
+    EditText testing;
+    ImageView pic;
+    FirebaseAuth mAuth;
+    Button btnLogout;
+    TextView textView;
+    FirebaseUser user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-
-        EditText testing;
-        ImageView pic;
-
-        testing=findViewById(R.id.editTxt1);
-
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -31,6 +37,33 @@ public class MainActivity extends AppCompatActivity {
 
 
         });
+
+        testing=findViewById(R.id.editTxt1);
+        pic=findViewById(R.id.imageView);
+        btnLogout=findViewById(R.id.btnLogout);
+        textView=findViewById(R.id.textViewUserDetails);
+        mAuth = FirebaseAuth.getInstance();
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user == null) {
+            Intent i = new Intent(MainActivity.this, Login.class);
+            startActivity(i);
+            finish();
+
+        }
+        else{
+            textView.setText(user.getEmail());
+        }
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                Intent i = new Intent(MainActivity.this, Login.class);
+                startActivity(i);
+                finish();
+            }
+        });
+
+
 
     }
 }
--- app/src/main/java/com/example/mobprogproject/register.java
+++ app/src/main/java/com/example/mobprogproject/register.java
@@ -23,35 +23,32 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class Login extends AppCompatActivity {
-
+public class register extends AppCompatActivity {
 
     EditText usernameEditText, passwordEditText;
-    Button btnLogin;
-    TextView textView;
+    Button btnRegister;
     FirebaseAuth mAuth;
     ProgressBar progressBar;
+    TextView textView;
 
 
     @Override
     public void onStart() {
         super.onStart();
         // Check if user is signed in (non-null) and update UI accordingly.
-        mAuth = FirebaseAuth.getInstance();
         FirebaseUser currentUser = mAuth.getCurrentUser();
         if(currentUser != null){
-            Intent i = new Intent(Login.this, MainActivity.class);
+            Intent i = new Intent(register.this, MainActivity.class);
             startActivity(i);
             finish();
         }
     }
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_register);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -60,20 +57,21 @@ public class Login extends AppCompatActivity {
 
         usernameEditText = findViewById(R.id.nameEditText);
         passwordEditText = findViewById(R.id.passwordEditText);
-        btnLogin = findViewById(R.id.btnLogin);
-        textView = findViewById(R.id.registerNow);
+        btnRegister = findViewById(R.id.btnRegister);
+        mAuth = FirebaseAuth.getInstance();
         progressBar = findViewById(R.id.progressBar);
+        textView = findViewById(R.id.loginNow);
 
         textView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(Login.this, register.class);
+                Intent i = new Intent(register.this, Login.class);
                 startActivity(i);
                 finish();
             }
         });
 
-        btnLogin.setOnClickListener(new View.OnClickListener() {
+        btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 progressBar.setVisibility(View.VISIBLE);
@@ -81,45 +79,34 @@ public class Login extends AppCompatActivity {
                 email = String.valueOf(usernameEditText.getText());
                 password = String.valueOf(passwordEditText.getText());
 
-                if (TextUtils.isEmpty(email)) {
-                    progressBar.setVisibility(View.GONE);
-                    Toast.makeText(Login.this, "Enter email", Toast.LENGTH_SHORT).show();
+                if(TextUtils.isEmpty(email)){
+                    Toast.makeText(register.this, "Enter email", Toast.LENGTH_SHORT).show();
                     return;
 
                 }
 
-                if (TextUtils.isEmpty(password)) {
-                    progressBar.setVisibility(View.GONE);
-                    Toast.makeText(Login.this, "Enter password", Toast.LENGTH_SHORT).show();
-                    return;
+                if(TextUtils.isEmpty(password)){
+                    Toast.makeText(register.this, "Enter password", Toast.LENGTH_SHORT).show();
 
                 }
-                if (email != null && password != null) {
-                    mAuth.signInWithEmailAndPassword(email, password)
-                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                                @Override
-                                public void onComplete(@NonNull Task<AuthResult> task) {
-                                    progressBar.setVisibility(View.GONE);
-                                    if (task.isSuccessful()) {
-                                        Toast.makeText(Login.this, "Login Successful", Toast.LENGTH_SHORT).show();
-                                        Intent i = new Intent(Login.this, MainActivity.class);
-                                        startActivity(i);
-                                        finish();
-                                    } else {
-                                        // If sign in fails, display a message to the user.
-                                        Toast.makeText(Login.this, "Email or Passowrd Incorrect",
-                                                Toast.LENGTH_SHORT).show();
 
-                                    }
+                mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener( new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                progressBar.setVisibility(View.GONE);
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(register.this,"Account created",Toast.LENGTH_LONG).show();
+
+
+                                } else {
+
+                                    Toast.makeText(register.this, "Account creation failed.",
+                                            Toast.LENGTH_SHORT).show();
                                 }
-                            });
-                }
+                            }
+                        });
             }
         });
-
-
-
-
-
     }
 }
